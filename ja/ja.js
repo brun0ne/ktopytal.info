ac9f571d25e2a4640dbbf6e789daff8bdd2b9432
BrunonDEV <43315279+BrunonDEV@users.noreply.github.com>
2023-02-21 15:58:34 +0100

    added /ja

---
+++ ja/ja.js
@@ -0,0 +1,54 @@
+let texts = [
+  "Ankietowanie 90% Polaków...",
+  "Zbieranie informacji z grupek na Facebooku...",
+  "Szukanie odpowiedzi wśród amerykańskich naukowców...",
+  "Analizowanie wiekowych tekstów kultury...",
+  "Rozwiązywanie ogromnych układów równań...",
+  "Przeszukiwanie wszystkich zakątków świata...",
+  "Proszenie o pomoc NASA...",
+  "Szukanie eksperta w tej dziedzinie na TikToku...",
+  'Wywieszanie ulotek "Zaginęła osoba, która pytała. Prosimy o kontakt"...',
+  "Robienie dobrych uczynków...",
+  "Bycie miłym dla losowo napotkanego człowieka...",
+  "Ładowanie mocy przyjaźni..."
+];
+
+function randomText(){
+  let index = Math.floor(Math.random() * texts.length);
+  let res = texts[index];
+
+  texts.splice(index, 1); // remove
+
+  return res;
+}
+
+function done(){
+  $("#pre-kto").fadeOut();
+  $("#after-kto").fadeIn();
+}
+
+$(document).ready(function(){
+  $("#how-long").html(((Math.random() * 100) / 100 + 10).toFixed(5));
+
+  // [sec]
+  const INTERVAL = 2;
+  const DURATION = 10
+
+  let is_done = false;
+
+  $("#progress-kto").animate({
+    width: "100%"
+  }, DURATION*1000);
+
+  setTimeout(function(){
+    done();
+    is_done = true;
+  }, DURATION*1000)
+
+  setInterval(function(){
+    if(is_done)
+      return false;
+
+    $("#text-random").html(randomText());
+  }, INTERVAL*1000);
+});
